d445a8c07f1c105a995df7e138c209d6de55f09d
Dima-Yu <mindwork.one@gmail.com>
2021-07-18 12:50:32 +0300

    add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name 
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonimus"
+  end
 end
